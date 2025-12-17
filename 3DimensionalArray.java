c6ab4218bbd27a86bb0ac3386e8f5d5dbc8be3f4
vallepugnanaprasanna-ship-it <vallepugnanaprasanna@gmail.com>
2025-12-17 11:47:47 +0530

    Add files via upload

---
+++ 3DimensionalArray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class 3DimensionalArray{
+public static void main(string[] args){
+        Scanner sc  = new Scanner(System.in);
+         int age[][]= new in[2][3][5];
+
+         for(int i=0; i<age.. length,i++){
+          for(int j=0;j<age[i].length;i++){
+                    
+          }
+         }
+}
+}
