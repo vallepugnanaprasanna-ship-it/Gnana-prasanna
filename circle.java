b56a00f09b893bfadb7020e5f57d3327d0684ef5
vallepugnanaprasanna-ship-it <vallepugnanaprasanna@gmail.com>
2025-12-17 11:49:48 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Circle{
+          int area(int r,float pi){
+                    return r*R*pi;
+          }
+          float area(int r, float pi){
+                    return r*r*pi;
+          }
+          double are(int r double pi){
+                    return r*r*pi;
+          }
+          public ExampleforMethods{
+                    System.out.print()
+          }
+}
